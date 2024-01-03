adf3014fa071063196de695430e5bf8407972b1a
DaisiSegun <daissegun096@gmail.com>
2024-01-03 02:40:20 +0100

    alrighty
    okayy

--- root-frontend/utils/newRequest.js
+++ root-frontend/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://api.roothq.africa",
+  baseURL: "https://busy-rose-nematode-sari.cyclic.app/api/",
   withCredentials: true,
 });
 
